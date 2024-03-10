6131a8ad31f02b536bcb5d1d082d98a869fb2e93
FELIXSEVENEIGHTNINE <119722813+FELIXSEVENEIGHTNINE@users.noreply.github.com>
2024-03-10 20:20:29 +0800

    1

--- process-form.php
+++ process-form.php
@@ -1,3 +1,14 @@
 <?php
 
-print_r($_POST);
+$name = $_POST["name"];
+$message = $_POST["message"];
+$priority = $_POST["priority"];
+$type = $_POST["type"];
+$terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOL);
+
+//$_POST["terms"];
+
+var_dump($name,$message,$priority,$type,$terms);
+
+
+//print_r($_POST);
